8ba595495a6262ac3984b8c9e18317feaa82f4f4
Hamroliyev <90793925+Hamroliyev@users.noreply.github.com>
2022-01-08 22:40:17 +0300

    Add Method GetDataAsJson and fill method

--- ValyutaViewJsonExcample/Program.cs
+++ ValyutaViewJsonExcample/Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
 
 namespace ValyutaViewJsonExcample
 {
     class Program
     {
+        static List<>
         static void Main(string[] args)
         {
+            string path = "https://nbu.uz/uz/exchange-rates/json/";
+            
+
+
+
+        }
+        static string GetDataAsJson(string url)
+        {
+            HttpClient client = new HttpClient();
+
+            string response = client.GetStringAsync(url).Result;
+
+            return response;
         }
     }
 }

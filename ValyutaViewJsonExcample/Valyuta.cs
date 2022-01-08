f10e06b107850e895a6463d40a2e18e1a6cc988c
Hamroliyev <90793925+Hamroliyev@users.noreply.github.com>
2022-01-08 22:39:15 +0300

    Add valyuta class and fill valyuta class

---
+++ ValyutaViewJsonExcample/Valyuta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValyutaViewJsonExcample
+{
+    public class Valyuta
+    {
+        public string Title { get; set; }
+
+        public string Code { get; set; }
+
+        public double Cb_price { get; set; }
+
+        public DateTime Date { get; set; }
+
+    }
+}
